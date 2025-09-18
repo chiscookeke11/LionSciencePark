661eef3db2b46fa4338af27c05ccc61cd7c8ab37
chiscookeke11 <chiscookeke11@gmail.com>
2025-09-18 11:15:40 +0100

    navbar setup completed

---
+++ src/Data/navlinksData.ts
@@ -0,0 +1,26 @@
+
+
+
+export const navLinkData = [
+    {
+        label: "HOME",
+        url: "/"
+    },
+    {
+        label: "ABOUT",
+        url: "/about"
+    },
+    {
+        label: "SERVICES",
+        url: "/services"
+    },
+    {
+        label: "TEAM",
+        url: "/team"
+    },
+    {
+        label: "NEWS",
+        url: "/news"
+    },
+
+]
---
+++ src/components/UI/Navbar.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { navLinkData } from "@/Data/navlinksData";
+import { Menu, X } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
+
+
+
+
+
+
+export default function Navbar() {
+    const pathName = usePathname()
+    const [showMobileMenu, setShowMobileMenu] = useState(false)
+
+
+    useEffect(() => {
+        document.body.style.overflowY = showMobileMenu ? "hidden" : "auto"
+    }, [])
+
+
+
+
+
+    return (
+        <nav className="w-full fixed top-0 left-0 bg-[#081623] py-4 px-4 lg:px-10 lg:pr-20  flex items-center justify-between font-poppins " >
+            <Link href={"/"} >
+                <Image src={"/logo/LSP-logo-blue-removeBG.png"} alt="logo" height={10} width={150} className="w-[100px] h-[50px] md:w-[150px] md:h-[60px] object-center object-cover  " />
+            </Link>
+
+
+            <ul className="hidden lg:flex items-center gap-10  " >
+                {navLinkData.map((navlink, index) => (
+                    <li key={index} className={` font-medium text-base ${pathName === navlink.url ? "text-[#4ab4b8]" : "text-[var(--foreground)]"}  hover:text-[#4ab4b8] duration-300 ease-in-out transition-colors   `} ><Link href={navlink.url} className=""  >{navlink.label}</Link> </li>
+                ))}
+            </ul>
+
+            <button className="text-[#4ab4b8] lg:hidden " onClick={() => setShowMobileMenu(true)} ><Menu size={33} /> </button>
+
+
+
+
+
+            <div className={`h-screen fixed top-0 left-0 w-full bg-[#081623] z-50  duration-300 ease-in-out transition-all py-6 px-7 flex flex-col items-start gap-7 ${showMobileMenu ? "translate-x-0" : "translate-x-[100%]"} `} >
+                <button className="text-[#4ab4b8] lg:hidden ml-auto " onClick={() => setShowMobileMenu(false)} ><X size={38} /> </button>
+
+                <ul className="flex flex-col items-start gap-10  " >
+                    {navLinkData.map((navlink, index) => (
+                        <li key={index} className={` font-medium text-xl ${pathName === navlink.url ? "text-[#4ab4b8]" : "text-[var(--foreground)]"}  hover:text-[#4ab4b8] duration-300 ease-in-out transition-colors    `} ><Link href={navlink.url} onClick={() => setShowMobileMenu(false)}  >{navlink.label}</Link> </li>
+                    ))}
+                </ul>
+            </div>
+
+        </nav>
+    )
+}
