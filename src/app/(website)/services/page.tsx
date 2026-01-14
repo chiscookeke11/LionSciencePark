b9a285d9a7cd22fe5e9cf55271eecc8966914d0a
chiscookeke11 <chiscookeke11@gmail.com>
2026-01-14 10:53:23 +0100

    updates on the service page

--- src/app/(website)/services/page.tsx
+++ src/app/(website)/services/page.tsx
@@ -9,18 +9,16 @@ export default function Page() {
         <div className="bg-white font-inter" >
 
             {/* service hero section  */}
-            <section className=" w-full h-fit flex items-center justify-center flex-col gap-12 py-20 px-3 text-center  font-inter " >
+            <section className=" w-full h-[70vh] bg-black flex items-center justify-center flex-col gap-4 text-white  py-4 px-3 text-center bg-no-repeat bg-center bg-cover relative overflow-hidden bg-fixed " style={{ backgroundImage: "url('/about-us/side.webp')" }} >
+                <div className=" bg-black/45 absolute inset-0 h-full w-full " />
 
-                <div className=" flex flex-col gap-3 z-30   " >
-                    <h2 className="  text-3xl md:text-6xl lg:text-7xl font-bold font-onest " >Our Services</h2>
-                    <p className="  text-lg md:text-2xl max-w-2xl " >Explore the wide range of innovative solutions and expertise we offer at Lion Science Park.</p>
+                <div className=" flex flex-col gap-3 z-30 " >
+                    <h2 className=" font-onest text-4xl md:text-8xl font-bold  " >Our Services</h2>
+                    <p className="font-inter  text-lg md:text-2xl max-w-2xl " >Explore the wide range of innovative solutions and expertise we offer at Lion Science Park.</p>
                 </div>
 
 
-                <div className="w-full max-w-xs md:max-w-3xl lg:max-w-6xl h-75 md:h-125 lg:h-170 relative rounded-2xl border-2 border-black overflow-hidden "  >
-                    <Image src={"/about-us/side.webp"} alt="image" height={1500} width={1500} className=" w-full h-full object-center object-cover " />
-                </div>
-
+                <Image src={"/service-images/wave-haikei.svg"} alt="wave" width={500} height={500} className="w-full absolute bottom-0 lg:bottom-[-200px] left-0 " />
             </section>
 
 
