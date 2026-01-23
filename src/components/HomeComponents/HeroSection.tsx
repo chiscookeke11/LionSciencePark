0044cb4044862fd2cb1dcdd4fc7136af8fa5fbfe
chiscookeke11 <chiscookeke11@gmail.com>
2026-01-23 06:46:43 +0100

    ui fixes

--- src/components/HomeComponents/HeroSection.tsx
+++ src/components/HomeComponents/HeroSection.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image';
 import React from 'react';
 import { motion } from "framer-motion"
+import Link from 'next/link';
 
 
 
@@ -36,17 +37,18 @@ export default function HeroSection() {
                     </motion.span>
                 </p>
 
-                <div className=" w-[250px] h-[60px] relative flex items-center justify-center rounded-lg group " >
-                    <div className="bg-black w-full h-full absolute top-0 left-0 rounded-lg group-hover:left-2 group-hover:top-2 transition-all duration-300 ease-in-out "  ></div>
-                    <button className=" text-white  cursor-pointer bg-[#008CC1] w-full h-full flex items-center justify-center rounded-lg py-3 px-5 absolute top-0 left-0 font-semibold tracking-wider group-hover:bg-[#00BFA6] border-2 border-transparent group-hover:border-black transition-all duration-300 ease-in-out " > Learn More</button>
-                    <motion.span
-                        initial={{ width: "100%" }}
-                        animate={{ width: 0 }}
-                        transition={{ duration: 0.4, ease: "easeInOut", delay: 1}}
-                        className="  h-full bg-white absolute top-0 left-0 " >
+                <Link href={"/about"} className='w-fit ' >
+                    <div className=" w-[250px] h-[60px] relative flex items-center justify-center rounded-lg group " >
+                        <div className="bg-black w-full h-full absolute top-0 left-0 rounded-lg group-hover:left-2 group-hover:top-2 transition-all duration-300 ease-in-out "  ></div>
+                        <button className=" text-white  cursor-pointer bg-[#008CC1] w-full h-full flex items-center justify-center rounded-lg py-3 px-5 absolute top-0 left-0 font-semibold tracking-wider group-hover:bg-[#00BFA6] border-2 border-transparent group-hover:border-black transition-all duration-300 ease-in-out " > Learn More</button>
+                        <motion.span
+                            initial={{ width: "100%" }}
+                            animate={{ width: 0 }}
+                            transition={{ duration: 0.4, ease: "easeInOut", delay: 1 }}
+                            className="  h-full bg-white absolute top-0 left-0 " >
 
-                    </motion.span>
-                </div>
+                        </motion.span>
+                    </div></Link>
             </div>
 
 
